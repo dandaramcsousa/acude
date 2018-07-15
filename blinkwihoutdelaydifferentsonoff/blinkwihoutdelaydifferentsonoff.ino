0539a97401c126f8a47d7dbda319aa23d9eb67f6
Dandara Sousa <dandara.sousa@ccc.ufcg.edu.br>
2018-07-15 20:53:05 -0300

    nova versao de blink com on e off diferentes e coordenador por ir

--- blinkwihoutdelaydifferentsonoff/blinkwihoutdelaydifferentsonoff.ino
+++ blinkwihoutdelaydifferentsonoff/blinkwihoutdelaydifferentsonoff.ino
@@ -1,4 +1,4 @@
-// 1: 16582903 2: 16615543 3: 16599223 4: 16591063 5: 16623703 
+// 1: 16582903 2: 16615543 3: 16599223 4: 16591063 5: 16623703
 // 6: 16607383 7: 16586983 8: 16619623 9: 16603303 0: 16593103
 // vol+: 16613503 vol-: 16617583
 #include <IRremote.h>
@@ -30,48 +30,48 @@ void setup() {
   irrecv.enableIRIn();
   Serial.println("Enabled IRin");
   // set the digital pin as output:
-  pinMode(irLEDPin1, OUTPUT);     
+  pinMode(irLEDPin1, OUTPUT);
 }
 
 void loop()
 {
-  if (irrecv.decode(&result)) 
+  if (irrecv.decode(&result))
   {
     Serial.println(result.value);
     irrecv.resume();
     switch (result.value) {
-      case 16582903: 
-        Serial.println("Botão 1"); 
-      	break;
-    
-    delay(1);
-  }
-  // here is where you'd put code that needs to be running all the time.
+      case 16582903:
+        Serial.println("Botão 1");
+        break;
 
-  // check to see if it's time to blink the LED; that is, if the
-  // difference between the current time and last time you blinked
-  // the LED is bigger than the interval at which you want to
-  // blink the LED.
-  unsigned long currentMillis = millis();
-
-  if(currentMillis - previousMillis > interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;   
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW)
-    {
-      interval = ONTIME;
-      ledState = HIGH;
+        delay(1);
     }
-    else
-    {
-     interval = OFFTIME;
-     ledState = LOW;
+    // here is where you'd put code that needs to be running all the time.
+
+    // check to see if it's time to blink the LED; that is, if the
+    // difference between the current time and last time you blinked
+    // the LED is bigger than the interval at which you want to
+    // blink the LED.
+      unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis > interval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW)
+      {
+        interval = ONTIME;
+        ledState = HIGH;
+      }
+      else
+      {
+        interval = OFFTIME;
+        ledState = LOW;
+      }
+      // set the LED with the ledState of the variable:
+      digitalWrite(irLEDPin1, ledState);
     }
-    // set the LED with the ledState of the variable:
-    digitalWrite(irLEDPin1, ledState);
   }
 }
 
-  
---
+++ blinkwithir/blinkwithir.ino
@@ -0,0 +1,89 @@
+#include <IRremote.h>
+
+int IRPIN = 4; //pino do receptor IR
+
+// leds controláveis
+int irLEDPin[6] = {8,9,10,11,12,13};
+
+// Variables will change:
+int ledState[6] = {LOW, LOW, LOW, LOW, LOW, LOW}; // ledState used to set the LED
+long previousMillis[6] = {0,0,0,0,0,0};        // will store last time LED was updated
+
+long interval[6] = {0,0,0,0,0,0};           // interval at which to blink (milliseconds)
+const unsigned long OFFTIME = 5000UL;
+unsigned long ONTIME[6] = {3000UL, 3000UL, 3000UL, 3000UL, 3000UL, 3000UL};
+
+IRrecv irrecv(IRPIN);
+
+decode_results result;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn();
+  Serial.println("Enabled IRin");
+ 
+  // set the digital pin as output:
+  pinMode(irLEDPin[0], OUTPUT);
+  pinMode(irLEDPin[1], OUTPUT);
+  pinMode(irLEDPin[2], OUTPUT);
+  pinMode(irLEDPin[3], OUTPUT);
+  pinMode(irLEDPin[4], OUTPUT);
+  pinMode(irLEDPin[5], OUTPUT);
+}
+
+void toggleLED (int n, unsigned long currentMillis )
+  {
+    previousMillis[n] = currentMillis;
+
+    if (ledState[n] == LOW)
+    {
+      interval[n] = ONTIME[n];
+      ledState[n] = HIGH;
+    }
+    else
+    {
+      interval[n] = OFFTIME;
+      ledState[n] = LOW;
+    }
+    digitalWrite(irLEDPin[n], ledState[n]);  
+  }
+
+void loop()
+{
+  if (irrecv.decode(&result))
+  {
+    Serial.println(result.value);
+    irrecv.resume();
+    switch (result.value) {
+      case 16582903:
+        Serial.println("Botão 1");
+        break;
+
+        delay(1);
+    }
+  }
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis[0] > interval[0]) {
+  toggleLED (0, currentMillis);
+  }
+  if (currentMillis - previousMillis[1] > interval[1]) {
+    toggleLED (1, currentMillis);
+  }
+  if (currentMillis - previousMillis[2] > interval[2]) {
+    toggleLED (2, currentMillis);
+  }
+  if (currentMillis - previousMillis[3] > interval[3]) {
+    toggleLED (3, currentMillis);
+  }
+  if (currentMillis - previousMillis[4] > interval[4]) {
+    toggleLED (4, currentMillis);
+  }
+  if (currentMillis - previousMillis[5] > interval[5]) {
+    toggleLED (5, currentMillis);
+  }
+}
+
+
+
